25368815210a7d4354dbce792247f0f82916f95c
Holly Vose <holly.vose@cuanschutz.edu>
2025-11-04 09:52:29 -0700

    black formatting all standalone files

--- standalone/HeatMap_standalone.py
+++ standalone/HeatMap_standalone.py
@@ -15,34 +15,55 @@ import sys
 ####################################################################################################
 # PUT ALL VARIABLES FROM YOUR DATASET HERE! THERE IS NO NEED TO EDIT THE CODE BELOW #
 
-#Your Filename
+# Your Filename
 filename = "../datasets/yHV68_MHVY_chal_Rep1.csv"
 format_based_on_filename = False
 alternate_title = "MHVY_NoThreshold_Rep1_heatmap"
 
-#quality control and formatting
-well_positions = False #if you only have well positions and want the heatmap plotted per well
-q_filtering = False #filter cell values based on quantile thresholing
-q_val = 0.95 #if q filtering, the threshold to set the filter to
-debug_show_plot = False #set to True if you want to view the plot locally. May break plot save.
+# quality control and formatting
+well_positions = (
+    False  # if you only have well positions and want the heatmap plotted per well
+)
+q_filtering = False  # filter cell values based on quantile thresholing
+q_val = 0.95  # if q filtering, the threshold to set the filter to
+debug_show_plot = (
+    False  # set to True if you want to view the plot locally. May break plot save.
+)
 
-#The order of tissues to plot on the graph (these must match the names in your file EXACTLY)
-TissueOrder = ["Adipose Tissue Mat","Cecum","Distal Colon","Liver","mLN","Omentum","PP","Proximal Colon","SI Zone A","SI Zone B","SI Zone C","SI Zone D","SI Zone E","Spleen"]	
+# The order of tissues to plot on the graph (these must match the names in your file EXACTLY)
+TissueOrder = [
+    "Adipose Tissue Mat",
+    "Cecum",
+    "Distal Colon",
+    "Liver",
+    "mLN",
+    "Omentum",
+    "PP",
+    "Proximal Colon",
+    "SI Zone A",
+    "SI Zone B",
+    "SI Zone C",
+    "SI Zone D",
+    "SI Zone E",
+    "Spleen",
+]
 
-#Treatment/Infection Order (these must match the secondary names in your file EXACTLY)
-TreatmentOrder = ["Uninfected","MHV-Y","yHV68","yHV68 + MHV-Y"]
+# Treatment/Infection Order (these must match the secondary names in your file EXACTLY)
+TreatmentOrder = ["Uninfected", "MHV-Y", "yHV68", "yHV68 + MHV-Y"]
 
-#Color Customization (sets the max value for the heatmap and generates a colormap)
-thresholding = False #determines if all values under a certain threshold should be the same color
+# Color Customization (sets the max value for the heatmap and generates a colormap)
+thresholding = (
+    False  # determines if all values under a certain threshold should be the same color
+)
 threshold = 1.32
 top_color = "#bb334c"
 
-#x and y axis data (these must match your column names EXACTLY)
+# x and y axis data (these must match your column names EXACTLY)
 x_val = "Tissue"
 heat_val = "MHV-Y"
 y_val = "Infection"
 
-#plot formatting
+# plot formatting
 title = "MHVY No Threshold"
 rotate_x = 90
 rotate_y = 0
@@ -52,29 +73,45 @@ rotate_y = 0
 # SET BASIC THEME PARAMETERS
 sns.set_theme()
 
+
 # FUNCTION DEFINITIONS
-def my_output_file(filename: str, plot_type: str ="Plot", extension: str="svg", csv:bool=True) -> str:
+def my_output_file(
+    filename: str, plot_type: str = "Plot", extension: str = "svg", csv: bool = True
+) -> str:
     """Creates a regex to rename the output file based on the original .csv file. The plot type adds the name of
-       the plot to the filename, and the extension specifies what file format to save (svg, png, jpeg, or pdf)."""
+    the plot to the filename, and the extension specifies what file format to save (svg, png, jpeg, or pdf).
+    """
     try:
         if extension in ["svg", "png", "pdf", "jpeg", "jpg"]:
             just_name = filename.split("/")
             if csv == True:
-                new_name = re.sub(".csv$","_Image" + plot_type + "." + extension, just_name[::-1][0],1)
+                new_name = re.sub(
+                    ".csv$",
+                    "_Image" + plot_type + "." + extension,
+                    just_name[::-1][0],
+                    1,
+                )
             else:
                 new_name = filename + "_Image" + plot_type + "." + extension
             return os.getcwd() + "/" + new_name
     except AttributeError:
-        print("_io.TextIOWrapper object has no attribute 'split'. Double check that the filename passed is a string.")
+        print(
+            "_io.TextIOWrapper object has no attribute 'split'. Double check that the filename passed is a string."
+        )
         sys.exit(1)
 
+
 # OPEN FILES AND GENERATE PATH NAMES
-myCSV = filename            # use this if you have access to this script directly
+myCSV = filename  # use this if you have access to this script directly
 
 if format_based_on_filename == True:
-    mySVGOut = my_output_file(filename, plot_type="Heatmap",extension="svg")  #Generates a regular expression to automate the output filename
+    mySVGOut = my_output_file(
+        filename, plot_type="Heatmap", extension="svg"
+    )  # Generates a regular expression to automate the output filename
 else:
-    mySVGOut = my_output_file(alternate_title, plot_type="Heatmap",extension="svg",csv=False)
+    mySVGOut = my_output_file(
+        alternate_title, plot_type="Heatmap", extension="svg", csv=False
+    )
 
 
 # READ IN THE DATA
@@ -88,43 +125,44 @@ if well_positions == True:
     DataSet["column"] = DataSet["Well Positions"].str.extract("(\d+)").astype(int)
 
     vmax_val = math.ceil(DataSet[heat_val].max())
-    heatmap_data = DataSet.pivot(index="row",columns="column",values=heat_val)
+    heatmap_data = DataSet.pivot(index="row", columns="column", values=heat_val)
 else:
     if q_filtering == True:
         q = DataSet[heat_val].quantile(q_val)
         DataSet = DataSet[DataSet[heat_val] < q]
-    
+
     vmax_val = DataSet[heat_val].max()
-    means = DataSet.groupby([x_val,y_val]).mean().reset_index()
-    heatmap_data = means.pivot(index=x_val,columns=y_val,values=heat_val)
+    means = DataSet.groupby([x_val, y_val]).mean().reset_index()
+    heatmap_data = means.pivot(index=x_val, columns=y_val, values=heat_val)
 
 
 # SET AESTHETICS
 Pal = sns.light_palette("#bb334c", as_cmap=True)
 
 if thresholding == False:
-    vmin_val = 0 
+    vmin_val = 0
 else:
     vmin_val = threshold
     Pal.set_under(color="#ffffff")
 
 
-#lut = dict(zip(TnType.unique(), "rbg"))
-#row_col = TnType.map(lut)
-#Sets color palette to be in the range from White to a Saturated Color, should be set to the color of the year
+# lut = dict(zip(TnType.unique(), "rbg"))
+# row_col = TnType.map(lut)
+# Sets color palette to be in the range from White to a Saturated Color, should be set to the color of the year
 
 # GENERATE THE HEATMAP
-g = sns.heatmap(heatmap_data,
-			cmap = Pal, #Determines the colormap based on a provided palette (see above)
-			vmin = vmin_val,  #Sets the minimum value for the lowest saturation of the color bar
-			vmax = vmax_val,  #Sets the maximum value for the highest saturation of the color bar
-            linewidths=.5,
-            xticklabels=True,
-            yticklabels=True
+g = sns.heatmap(
+    heatmap_data,
+    cmap=Pal,  # Determines the colormap based on a provided palette (see above)
+    vmin=vmin_val,  # Sets the minimum value for the lowest saturation of the color bar
+    vmax=vmax_val,  # Sets the maximum value for the highest saturation of the color bar
+    linewidths=0.5,
+    xticklabels=True,
+    yticklabels=True,
 )
 
 # OUTPUT AND SAVE THE PLOT
-#g.ax_row_dendrogram.remove()    #removes the dendrogram from the plot (if needed)
+# g.ax_row_dendrogram.remove()    #removes the dendrogram from the plot (if needed)
 plt.setp(g.get_xticklabels(), rotation=rotate_x)
 plt.setp(g.get_yticklabels(), rotation=rotate_y)
 plt.tight_layout()
@@ -133,4 +171,4 @@ plt.title(title)
 if debug_show_plot == True:
     plt.show()
 fig = g.figure
-fig.savefig(mySVGOut,bbox_inches="tight")
+fig.savefig(mySVGOut, bbox_inches="tight")
--- standalone/MakeViolinPlot_standalone.py
+++ standalone/MakeViolinPlot_standalone.py
@@ -17,87 +17,106 @@ filename = "../datasets/my_file.csv"
 
 debug_show_plot = False
 
-TissueOrder = ["mLN","PeyersPatch","Colon"]	
-TreatmentOrder = ["MHV-Y", "MHV-Y_dHE"]  
+TissueOrder = ["mLN", "PeyersPatch", "Colon"]
+TreatmentOrder = ["MHV-Y", "MHV-Y_dHE"]
 
 x_val = "Tissue"
 y_val = "Log_Copies"
 hue_val = "Treatment"
 
 white_overlay = "Treatment"
-custom_colors = ["#AE3899","#CF92DD","#009933","#EDAB21"]
+custom_colors = ["#AE3899", "#CF92DD", "#009933", "#EDAB21"]
 
 ####################################################################################################
 
+
 # FUNCTION DEFINITIONS
-def my_output_file(filename: str, plot_type: str ="Plot", extension: str="svg", csv:bool=True) -> str:
+def my_output_file(
+    filename: str, plot_type: str = "Plot", extension: str = "svg", csv: bool = True
+) -> str:
     """Creates a regex to rename the output file based on the original .csv file. The plot type adds the name of
-       the plot to the filename, and the extension specifies what file format to save (svg, png, jpeg, or pdf)."""
+    the plot to the filename, and the extension specifies what file format to save (svg, png, jpeg, or pdf).
+    """
     try:
         if extension in ["svg", "png", "pdf", "jpeg", "jpg"]:
             just_name = filename.split("/")
             if csv == True:
-                new_name = re.sub(".csv$","_Image" + plot_type + "." + extension, just_name[::-1][0],1)
+                new_name = re.sub(
+                    ".csv$",
+                    "_Image" + plot_type + "." + extension,
+                    just_name[::-1][0],
+                    1,
+                )
             else:
                 new_name = filename + "_" + plot_type + "." + extension
             return os.getcwd() + "/" + new_name
     except AttributeError:
-        print("_io.TextIOWrapper object has no attribute 'split'. Double check that the filename passed is a string.")
+        print(
+            "_io.TextIOWrapper object has no attribute 'split'. Double check that the filename passed is a string."
+        )
         sys.exit(1)
 
 
 # READ IN THE DATA
-DataSet = pd.read_csv(filename,index_col = 0)
-mySVGOut = my_output_file(filename, plot_type="Violin",extension="svg")  #regex to generate a new filename based on the .csv filename
+DataSet = pd.read_csv(filename, index_col=0)
+mySVGOut = my_output_file(
+    filename, plot_type="Violin", extension="svg"
+)  # regex to generate a new filename based on the .csv filename
 
 
 # GET COLORS AND DEFINTE AESTHETIC PARAMETERS
-sns.set_theme(rc={'figure.figsize':(5,2)}) #sets the global parameters for graphing objects
+sns.set_theme(
+    rc={"figure.figsize": (5, 2)}
+)  # sets the global parameters for graphing objects
 WhiteWheel = ["#FFFFFF"] * len(list(set(DataSet[white_overlay])))
 
 # MAKE THE VIOLIN PLOT
-g = sns.violinplot(data=DataSet, #data: data object being plotted
-	x=x_val,                  #x: x-axis label
-	y=y_val,              #y: y-axis label
-	hue=hue_val,	         #hue: defines how the bars will be split
-	cut = 1,                     #cut: defines how far the density extends past the data point extremes (0 limits to inside the points)
-	order = TissueOrder,	     #order: defines the order of the tissues on the x axis
-	hue_order = TreatmentOrder,  #hue_order: defines the order of the treatment on the X axis
-	palette= custom_colors,         #palette: controls the colors on the graph, must be a hash Treatment value to Hex code
-	saturation = 1,              #saturation: defines how saturated the color will be (0 is black and white, 1 is fully colored)
-#	height=1,		             #height: controls the height of the graph
-	inner = None,                #inner: can specify a smaller graph type behind the data (i.e. "box", "stick", etc...)
-	density_norm = 'width',      #density_norm: conforms all plots to the same parameter (width = they all have the same width)
-#	aspect = 1, #3 Categories    #aspect: controls the width/height ratio of the graph, relevant to how many graphs there are
-#	aspect = .7, #2 Categories
-	linewidth = 1,               #linewidth: defines the line thickness around the bar
-	edgecolor = "black"	         #edgecolor: defines the line color around the bar
+g = sns.violinplot(
+    data=DataSet,  # data: data object being plotted
+    x=x_val,  # x: x-axis label
+    y=y_val,  # y: y-axis label
+    hue=hue_val,  # hue: defines how the bars will be split
+    cut=1,  # cut: defines how far the density extends past the data point extremes (0 limits to inside the points)
+    order=TissueOrder,  # order: defines the order of the tissues on the x axis
+    hue_order=TreatmentOrder,  # hue_order: defines the order of the treatment on the X axis
+    palette=custom_colors,  # palette: controls the colors on the graph, must be a hash Treatment value to Hex code
+    saturation=1,  # saturation: defines how saturated the color will be (0 is black and white, 1 is fully colored)
+    # 	height=1,		             #height: controls the height of the graph
+    inner=None,  # inner: can specify a smaller graph type behind the data (i.e. "box", "stick", etc...)
+    density_norm="width",  # density_norm: conforms all plots to the same parameter (width = they all have the same width)
+    # 	aspect = 1, #3 Categories    #aspect: controls the width/height ratio of the graph, relevant to how many graphs there are
+    # 	aspect = .7, #2 Categories
+    linewidth=1,  # linewidth: defines the line thickness around the bar
+    edgecolor="black",  # edgecolor: defines the line color around the bar
 )
-plt.ylim(0,7)                    #plt.ylim(min, max): limits the range on the y-axis 
-#sns.set_axis_labels("", "")
-#g.legend.set_title("")
+plt.ylim(0, 7)  # plt.ylim(min, max): limits the range on the y-axis
+# sns.set_axis_labels("", "")
+# g.legend.set_title("")
 
 
 # MAKE THE SWARM PLOT TO SHOW OBSERVATIONS
-ax = sns.swarmplot(data=DataSet,                #data object being graphed
-					x=x_val,                 #x-axis label
-					y=y_val,             #y-axis label
-					hue=hue_val,            #defines how the points will be split
-					order = TissueOrder,	    #defines the order of the tissues on the x axis
-					dodge = "true",	            #makes sure the dots are not plotted in the same column
-					hue_order = TreatmentOrder, #defines the order of the treatment on the X axis
-					palette = WhiteWheel,       #defines color palette to make sure every dot is white
-					edgecolor = "black",        #colors the edge of the points
-					size = 5,	                #defines size of dot
-					linewidth = .75, #defines the line thickness around the white dot
+ax = sns.swarmplot(
+    data=DataSet,  # data object being graphed
+    x=x_val,  # x-axis label
+    y=y_val,  # y-axis label
+    hue=hue_val,  # defines how the points will be split
+    order=TissueOrder,  # defines the order of the tissues on the x axis
+    dodge="true",  # makes sure the dots are not plotted in the same column
+    hue_order=TreatmentOrder,  # defines the order of the treatment on the X axis
+    palette=WhiteWheel,  # defines color palette to make sure every dot is white
+    edgecolor="black",  # colors the edge of the points
+    size=5,  # defines size of dot
+    linewidth=0.75,  # defines the line thickness around the white dot
 )
-#ax.set(ylim=(0, 6))
-#sns.despine(top = True)
-ax.legend_.remove()       #removes the legend from the graph
+# ax.set(ylim=(0, 6))
+# sns.despine(top = True)
+ax.legend_.remove()  # removes the legend from the graph
 
-#SHOW AND SAVE THE PLOT
-g = g.figure              #creates a figure object
+# SHOW AND SAVE THE PLOT
+g = g.figure  # creates a figure object
 
 if debug_show_plot == True:
-	plt.show()                #shows a preview output
-g.savefig(mySVGOut)      #saves the file to a .svg format with the regex filename from earlier
+    plt.show()  # shows a preview output
+g.savefig(
+    mySVGOut
+)  # saves the file to a .svg format with the regex filename from earlier
